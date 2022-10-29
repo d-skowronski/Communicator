8bf1fb52e13071a752b60d4212df9581af948781
Dawid Skowronski <dskwr@protonmail.com>
2022-10-29 19:56:12 +0200

    Email field was made unique

--- Chat/models.py
+++ Chat/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    
+    email = models.EmailField(max_length=64, unique=True)
+    username = models.CharField(max_length=32,unique=True)
     def __str__(self):
         return self.username
