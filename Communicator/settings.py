0a7dec0839cd9e8e6044c957a2bf206cc3752612
Dawid Skowronski <dskwr@protonmail.com>
2022-10-29 15:51:52 +0200

    Initial migration, custom User model created

--- Communicator/settings.py
+++ Communicator/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat'
+    'Chat'
 ]
 
 MIDDLEWARE = [
@@ -84,6 +84,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = "Chat.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
