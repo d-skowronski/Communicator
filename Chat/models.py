4c0913002bb6090a0ccea850c37366d8b724180e
Dawid Skowronski <dskwr@protonmail.com>
2022-11-07 13:20:34 +0100

    added profile pictures to User model

--- Chat/models.py
+++ Chat/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    profilePicture = models.ImageField(upload_to="profile_pictures", default='noimage.png')
     email = models.EmailField(max_length=64, unique=True)
     username = models.CharField(max_length=32,unique=True)
     def __str__(self):
--- Communicator/settings.py
+++ Communicator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.contrib import messages
 
@@ -142,3 +142,9 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
