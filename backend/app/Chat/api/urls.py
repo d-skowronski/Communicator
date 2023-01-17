1e1f652a85ce1b9f568716990da49026bf773f63
Dawid Skowronski <dskwr@protonmail.com>
2023-01-17 11:15:55 +0100

    renamed endpoints to plural nouns

--- backend/app/Chat/api/urls.py
+++ backend/app/Chat/api/urls.py
@@ -3,10 +3,10 @@ from . import views
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('room/', views.RoomList.as_view()),
-    path('room/<int:pk>/', views.RoomDetail.as_view()),
-    path('user/', views.UserList.as_view()),
-    path('user/<int:pk>/', views.UserDetail.as_view()),
+    path('rooms/', views.RoomList.as_view()),
+    path('rooms/<int:pk>/', views.RoomDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
     path('messages/', views.MessagesList.as_view()),
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
