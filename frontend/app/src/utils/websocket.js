dfcac26ea33756ee30a99b5cd65ec24b26cb6822
Dawid Skowronski <dskwr@protonmail.com>
2023-01-25 13:58:07 +0100

    adding new messages to query cache

--- frontend/app/src/utils/websocket.js
+++ frontend/app/src/utils/websocket.js
@@ -1,13 +1,28 @@
-import React, { useEffect } from 'react'
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react'
 
 export default function useWebsocket() {
+    const queryClient = useQueryClient()
+
     useEffect(() => {
         const ws = new WebSocket("ws://127.0.0.1:8000/ws/chat");
         console.log("ws ", ws)
         ws.onmessage = (event) => {
             const data = JSON.parse(event.data)
-            console.log(data)
+            console.log("WEBSOCKET INCOMING: ", data)
+            if (data.information_type === "chat_message"){
+                const message = data
+                queryClient.setQueryData(
+                    ['messages', `messages-room-${message.room}`],
+                    (oldData) => {
+                        if(oldData) {
+                            return {...oldData, results: [message, ...oldData.results]}
+                        }
+                        else return {results: [message]}
+                    }
+                )
+            }
         }
-        return ws.close()
-    }, [])
+
+    }, [queryClient])
 }
