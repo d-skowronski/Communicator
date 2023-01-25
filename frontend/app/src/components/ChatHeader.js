b147a3096d573fe338bbe381fbc15efb32820a61
Dawid Skowronski <dskwr@protonmail.com>
2023-01-25 19:05:05 +0100

    added image to room header

--- frontend/app/src/components/ChatHeader.js
+++ frontend/app/src/components/ChatHeader.js
@@ -3,7 +3,10 @@ import '../css/ChatHeader.css'
 
 function ChatHeader({ currentRoom }) {
     return (
-        <header className='chat-header'>{currentRoom?.name}</header>
+        <header className='chat-header'>
+            <img className='room-thumbnail' src={currentRoom.thumbnail} alt=""></img>
+            {currentRoom.name}
+        </header>
     )
 }
 
