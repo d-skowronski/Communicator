a0ca38bb82b7a96f1eb1d319ad8bdcb7b61a2ed0
Dawid Skowronski <dskwr@protonmail.com>
2023-01-25 10:44:03 +0100

    removed strict mode, fast solution to doubling ws

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -7,11 +7,9 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools/>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools/>
+  </QueryClientProvider>
 );
 
