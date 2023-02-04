ed3063fe78c01d2876d76977557e2a7b86b19f47
Dawid Skowronski <dskwr@protonmail.com>
2023-02-04 13:11:58 +0100

    basic user bar layout

--- frontend/app/src/components/UserBar.js
+++ frontend/app/src/components/UserBar.js
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react'
 import AuthContext from '../context/AuthContext'
 import '../css/UserBar.css'
+import '../css/GlobalStyles.css'
 
 
 function UserBar() {
-  const {logoutUser} = useContext(AuthContext)
+  const {logoutUser, user} = useContext(AuthContext)
+  // Temporary. TODO: Solution would be to serialize full url in backend
+  const src = "http://" + window.location.hostname + ":8000" + user.profile_picture
   return (
     <div className='user-bar'>
-        UserBar
-        <button onClick={logoutUser}>logout</button>
+        <img className='profile-pic' src={src}alt=""></img>
+        <div className='user-bar-name'>{user.username}</div>
+        <button onClick={logoutUser}><i class="bi bi-box-arrow-right"></i></button>
     </div>
   )
 }
