a15cf55380ea9617ffe6be8dd0bb1d6421d3a64c
Dawid Skowronski <dskwr@protonmail.com>
2023-02-04 13:32:33 +0100

    added overlay for profile info nav button

--- frontend/app/src/components/UserBar.js
+++ frontend/app/src/components/UserBar.js
@@ -1,18 +1,25 @@
 import React, { useContext } from 'react'
 import AuthContext from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 import '../css/UserBar.css'
 import '../css/GlobalStyles.css'
 
 
 function UserBar() {
   const {logoutUser, user} = useContext(AuthContext)
+  const navigate = useNavigate()
   // Temporary. TODO: Solution would be to serialize full url in backend
   const src = "http://" + window.location.hostname + ":8000" + user.profile_picture
   return (
     <div className='user-bar'>
+        <div className='profile-pic action-overlay' onClick={() => navigate('/communicator/profile')}>
+          <i class="bi bi-info-lg"></i>
+        </div>
         <img className='profile-pic' src={src}alt=""></img>
         <div className='user-bar-name'>{user.username}</div>
-        <button onClick={logoutUser}><i class="bi bi-box-arrow-right"></i></button>
+        <button onClick={logoutUser}>
+          <i class="bi bi-box-arrow-right"></i>
+        </button>
     </div>
   )
 }
