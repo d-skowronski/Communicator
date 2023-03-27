45002b97e48b4f15fc19b99e980880ac960015cf
Dawid Skowronski <dskwr@protonmail.com>
2023-03-27 12:41:46 +0200

    added tooltip to read icon

--- frontend/app/src/components/ReadIcon.js
+++ frontend/app/src/components/ReadIcon.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useQueryUser } from '../utils/hooks/queries'
 import '../css/ReadIcon.css'
+import { Tooltip } from 'react-tooltip'
 
 function ReadIcon({userId}) {
     const user = useQueryUser(userId)
   return (
-    <img className='read-icon profile-pic' src={user.profile_picture}></img>
+    <div className='read-icon'>
+      <img
+        className='profile-pic'
+        src={user.profile_picture}
+        alt={user.username}
+        data-tooltip-id={user.id}
+        data-tooltip-content={user.username}
+        data-tooltip-place="left"
+      ></img>
+      <Tooltip id={user.id} className='tooltip'/>
+    </div>
+
   )
 }
 
