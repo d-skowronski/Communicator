1aa4744940c066f6378b3487054cf3a6b6d10540
Dawid Skowronski <dskwr@protonmail.com>
2023-04-24 10:43:15 +0200

    superuser automaticly created only once

--- backend/app/createsuperuser.py
+++ backend/app/createsuperuser.py
@@ -1,14 +1,16 @@
-from django.db import IntegrityError
+'''
+Automatic super user creation script.
+Works only if no users in database. This is a security feature to
+make creating next super users intentional, not automatic.
+'''
 from django.contrib.auth import get_user_model
 from os import environ
 
-try:
-    User = get_user_model()
+User = get_user_model()
+if not User.objects.all():
     superuser = User.objects.create_superuser(
-        username=environ.get('SUPER_USER_NAME', default="admin"),
-        email=environ.get('SUPER_USER_EMAIL', default="admin@example.com"),
-        password=environ.get('SUPER_USER_PASSWORD', default="insecurepassword"))
+        username=environ.get('DJANGO_SUPERUSER_USERNAME', default="admin"),
+        email=environ.get('DJANGO_SUPERUSER_EMAIL', default="admin@example.com"),
+        password=environ.get('DJANGO_SUPERUSER_PASSWORD', default="insecurepassword"))
     superuser.save()
-    print(f"Super User with username {environ.get('SUPER_USER_NAME')} created")
-except IntegrityError:
-    print(f"Super User with username {environ.get('SUPER_USER_NAME')} exists!")
+    print(f"Super User with username {environ.get('DJANGO_SUPERUSER_USERNAME')} created")
